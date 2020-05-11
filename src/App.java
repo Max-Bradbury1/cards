22ea88a8c0b152741fa8b156f7b93e0378499084
tsinnott-vms <tsinnott@vms.edu>
2020-05-11 16:21:05 -0600

    Moved rank and suit variables so they are instance variables in App. Also added a convenience array variable called allRanks.

--- src/App.java
+++ src/App.java
@@ -2,27 +2,38 @@ import processing.core.PApplet;
 
 public class App extends PApplet
 {
+    private Rank r0 = new Rank ("Two", 2);
+    private Rank r1 = new Rank ("Three", 3);
+    private Rank r2 = new Rank ("Four", 4);
+    private Rank r3 = new Rank ("Five", 5);
+    private Rank r4 = new Rank ("Six", 6);
+    private Rank r5 = new Rank ("Seven", 7);
+    private Rank r6 = new Rank ("Eight", 8);
+    private Rank r7 = new Rank ("Nine", 9);
+    private Rank r8 = new Rank ("Ten", 10);
+    private Rank r9 = new Rank ("Jack", 11);
+    private Rank r10 = new Rank ("Queen", 12);
+    private Rank r11 = new Rank ("King", 13);
+    private Rank r12 = new Rank ("Ace", 14);
+    private Rank[] allRanks = {r0,r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,r11,r12};
+
+    private Suit diamonds = new Suit ("Diamonds",0);
+    private Suit hearts = new Suit ("Hearts", 0);
+    private Suit clubs = new Suit ("Clubs", 0);
+    private Suit spades = new Suit ("Spades", 0);
+
+    //Card c = new Card(r0, diamonds);
+
+    public void settings()
+    {
+        size(400,400);
+
+    }
+
+
     public static void main(String[] args)
     {
         PApplet.main("App");
-        Rank r0 = new Rank ("Two", 2);
-        Rank r1 = new Rank ("Three", 3);
-        Rank r2 = new Rank ("Four", 4);
-        Rank r3 = new Rank ("Five", 5);
-        Rank r4 = new Rank ("Six", 6);
-        Rank r5 = new Rank ("Seven", 7);
-        Rank r6 = new Rank ("Eight", 8);
-        Rank r7 = new Rank ("Nine", 9);
-        Rank r8 = new Rank ("Ten", 10);
-        Rank r9 = new Rank ("Jack", 11);
-        Rank r10 = new Rank ("Queen", 12);
-        Rank r11 = new Rank ("King", 13);
-        Rank r12 = new Rank ("Ace", 14);
-
-        Suit s0 = new Suit ("Diamonds",0);
-        Suit s1 = new Suit ("Hearts", 0);
-        Suit s2 = new Suit ("Clubs", 0);
-        Suit s3 = new Suit ("Spades", 0);
     }
 
 }
